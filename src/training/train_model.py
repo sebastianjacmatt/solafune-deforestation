8f814400ca84af7416fcfa677bc0d35235c46078
brageaasen <brageaasen@hotmail.com>
2025-01-21 15:31:10 +0100

    Added init solutions

---
+++ src/training/train_model.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+from config import MODELS_DIR
+
+def train():
+    # Placeholder for training
+    print("Training the model...")
+
+if __name__ == "__main__":
+    train()
