45054cce0b7c5fb7ff15b6bcab2885d9ad44b802
SebastianJM <buy16@uib.no>
2025-01-22 16:55:13 +0100

    added pillow as dependencies

--- src/preprocessing/preprocess_train.py
+++ src/preprocessing/preprocess_train.py
@@ -1,7 +1,3 @@
 from PIL import Image
-
-# Open the TIF image
-image = Image.open("data/train_images/train_0.tif")
-
-# Display the image
-image.show()
+with Image.open("../data/train_images/mushroom.jpg") as im:
+    im.show()
