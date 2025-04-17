25bcab6da1b79a192c8569456bb5f74a2128ea0f
brageaasen <brageaasen@hotmail.com>
2025-04-17 15:29:20 +0200

    Modulated generation of masks

---
+++ src/preprocessing/mask_generation/generate_masks.py
@@ -0,0 +1,9 @@
+from get_masks import get_and_save_masks
+from visualize_masks import visualize_masks
+
+def main():
+    get_and_save_masks()
+    visualize_masks()
+
+if __name__ == "__main__":
+    main()
